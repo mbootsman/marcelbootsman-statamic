f9a820d2ca01d4a32622de66bd94df8168566c94
Marcel Bootsman <marcel@nostromo.nl>
2023-09-12 17:14:04 +0200

    Added Torchlight stuff to make it work with SSG

---
+++ app/CustomGenerator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Statamic\StaticSite\Generator;
+
+class CustomGenerator extends Generator {
+    protected function createPage($content) {
+        return new CustomPage($this->files, $this->config, $content);
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,24 @@
 namespace App\Providers;
 
 use App\UrlPaginator;
+use App\CustomGenerator;
 use Illuminate\Support\ServiceProvider;
 use Studio1902\PeakSeo\Handlers\ErrorPage;
 use Statamic\Extensions\Pagination\LengthAwarePaginator;
 use Statamic\Facades\Entry;
+use Statamic\Facades\Markdown;
 use Statamic\StaticSite\Generator;
 use Statamic\StaticSite\SSG;
+use Statamic\StaticSite\Tasks;
+use Torchlight\Commonmark\V2\TorchlightExtension;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
     /**
      * Register any application services.
      *
      * @return void
      */
-    public function register()
-    {
+    public function register() {
         //
     }
 
@@ -27,17 +29,24 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
+    public function boot() {
         // Statamic::script('app', 'cp');
         // Statamic::style('app', 'cp');
 
         // Doesn't work with Peak at the moment
         // ErrorPage::handle404AsEntry();
 
+        Markdown::addExtension(function () {
+            return new TorchlightExtension;
+        });
+
+        $this->app->singleton(Generator::class, function ($app) {
+            return new CustomGenerator($app, $app['files'], $app['router'], $app[Tasks::class]);
+        });
+
         SSG::after(function () {
             $from = public_path('img');
-            $to = config('statamic.ssg.destination').'/img';
+            $to = config('statamic.ssg.destination') . '/img';
             app('files')->copyDirectory($from, $to);
         });
     }
